0bc135d789f8cf07e5409d26b9b4652ba0ae3f3e
Joy Kumar Acharjee <joy29940@gmail.com>
2024-07-03 07:40:32 +0600

    added post model

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    banner: {
+      type: String,
+    },
+    category: {
+      // TODO: need to be updated
+      type: String,
+      required: true,
+    },
+    keyword: {
+      type: [String],
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    upvotes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Upvote",
+      },
+    ],
+    downvotes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Downvote",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
