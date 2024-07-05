5f2c570a42fb717332f03be6b9a30ad4cd7484a7
Joy Kumar Acharjee <joy29940@gmail.com>
2024-07-05 23:04:30 +0600

    added remove follower feature

--- controllers/user/follower.js
+++ controllers/user/follower.js
@@ -40,14 +40,62 @@ const addFollowing = async (req, res, next) => {
   }
 };
 
-const removeFollowing = (req, res) => {
+const removeFollowing = async (req, res, next) => {
   try {
+    // Find logged user
+    const user_id = req.user;
+    const loggedUser = await User.findById(user_id);
+
+    if (!loggedUser) {
+      return next(appError("Please log in again", 401));
+    }
+
+    // Find the following user
+    const { id } = req.params;
+    const secondUser = await User.findById(id);
+
+    if (id === user_id) {
+      return next(appError("Not possible for same user", 400));
+    }
+
+    if (!secondUser) {
+      return next(appError("User Not Found", 404));
+    }
+
+    // Check the list
+    if (
+      loggedUser.following.findIndex(
+        (user) => JSON.stringify(user) === JSON.stringify(secondUser._id)
+      ) === -1
+    ) {
+      return next(appError("User not available in your following list", 404));
+    }
+
+    if (
+      secondUser.followers.findIndex(
+        (user) => JSON.stringify(user) === JSON.stringify(loggedUser._id)
+      ) === -1
+    ) {
+      return next(appError("User not available in followers list", 404));
+    }
+
+    // remove the user from follower and following list
+    loggedUser.following = loggedUser.following.filter(
+      (user) => JSON.stringify(user) !== JSON.stringify(secondUser._id)
+    );
+    await loggedUser.save();
+
+    secondUser.followers = secondUser.followers.filter(
+      (user) => JSON.stringify(user) !== JSON.stringify(loggedUser._id)
+    );
+    await secondUser.save();
+
     res.json({
       status: "success",
-      msg: "Following is removed",
+      msg: `You unfollowed ${secondUser.fullname}`,
     });
   } catch (error) {
-    console.log(error);
+    next(appError(error.message));
   }
 };
 
--- models/User.js
+++ models/User.js
@@ -73,6 +73,7 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
+    toJSON: { virtuals: true },
   }
 );
 
