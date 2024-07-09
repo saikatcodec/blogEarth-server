62d1e29894b1c57af8242260629091c129dc0944
Joy Kumar Acharjee <joy29940@gmail.com>
2024-07-09 14:48:21 +0600

    added upvote

--- controllers/upvote/upvoteControllers.js
+++ controllers/upvote/upvoteControllers.js
@@ -1,10 +1,43 @@
+const Post = require("../../models/Post");
+const Upvote = require("../../models/Upvote");
+const User = require("../../models/User");
 const appError = require("../../utils/appError");
 
-const addUpvote = (req, res, next) => {
+const addUpvote = async (req, res, next) => {
   try {
+    // Find id
+    const { post_id } = req.params;
+    const user_id = req.user;
+
+    // Find user from user id
+    const user = await User.findById(user_id);
+    if (!user) {
+      return next(appError("Invalid User. Please Log in", 401));
+    }
+
+    // Find post from post id
+    const post = await Post.findById(post_id);
+    if (!post) {
+      return next(appError("Post is not availble", 404));
+    }
+
+    const upvote = await Upvote.create({
+      author: user._id,
+      post: post._id,
+    });
+
+    // Add ref to user model
+    user.upvotes.push(upvote._id);
+    user.save();
+
+    // Add ref to post model
+    post.upvotes.push(upvote._id);
+    post.save();
+
     res.json({
       status: "success",
       msg: "Upvote added successfully",
+      data: upvote,
     });
   } catch (error) {
     next(appError(error.message));
--- models/Upvote.js
+++ models/Upvote.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const appError = require("../utils/appError");
 
 const upvoteSchema = new mongoose.Schema(
   {
@@ -6,12 +7,26 @@ const upvoteSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
+      unique: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
     },
   },
   {
     timestamps: true,
   }
 );
+
+upvoteSchema.post("save", (err, res, next) => {
+  if (err.name === "MongoServerError" && err.code === 11000) {
+    next(appError("You upvoted already.", 409));
+  } else {
+    next(err);
+  }
+});
 
 const Upvote = mongoose.model("Upvote", upvoteSchema);
 module.exports = Upvote;
